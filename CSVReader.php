971d76a0c749cfddb45452a9fceca2871e30db09
enfrte <enfrte@gmail.com>
2021-08-10 07:27:21 +0300

    Init and inital upload

---
+++ CSVReader.php
@@ -0,0 +1,116 @@
+<?php 
+// https://en.wikipedia.org/wiki/Comma-separated_values#Example
+// http://localhost/csv-reader/
+
+class CSVReader 
+{
+	protected $file;
+	protected $separator;
+	protected $tables;
+	protected $line_number;
+	protected $previous_table;
+	protected $end_of_file;	
+	
+	public function __construct($file, $separator)
+	{
+		$this->file = $file;
+		$this->separator = $separator;
+		$this->tables = new StdClass();
+	}
+
+	public function setTable($name, $line_number_start, $line_number_end = false) : self
+	{
+		$this->tables->$name = new StdClass();
+		$this->tables->$name->line_number_start = $line_number_start;
+		$this->previous_table = $name;
+		if ($line_number_end) {
+			$this->tables->$name->line_number_end = $line_number_end;
+		}
+		else { $this->tables->$name->line_number_end = $line_number_start; }
+		return $this;
+	}
+
+    public function setTableColumns($columns) : self
+    {
+		$this->tables->{$this->previous_table}->columns = $columns;
+		$this->populateTableData($this->previous_table);
+        return $this;
+    }
+
+	public function theEnd()
+	{
+		return $this->setLastLineOfFileNumber();
+	}
+
+	private function setLastLineOfFileNumber()
+	{
+		$linecount = 0;
+		$handle = fopen($this->file, "r");
+
+		while(!feof($handle)){
+			$line = fgets($handle);
+			$linecount++;
+		}
+
+		fclose($handle);
+		return $linecount;
+	}
+
+	public function populateTableData($table)
+	{
+		$this->tables->$table->column_data = [];
+		$line_number_start = $this->tables->$table->line_number_start;
+		$line_number_end = $this->tables->$table->line_number_end;
+		$row = 1;
+
+		if (($handle = fopen($this->file, "r")) !== FALSE) {
+			while (($line_data = fgetcsv($handle, 1000, $this->separator)) !== FALSE) {
+				$column_total = count($line_data); // number of columns
+				// Find the line range to start to copy data
+				if ($row >= $line_number_start && $row <= $line_number_end) {
+					for ($column_index = 0; $column_index < $column_total; $column_index++) {
+						$this->tables->$table->column_data[$row][$column_index] = $line_data[$column_index];
+					}
+				}
+				$row++;
+			}
+			fclose($handle);
+		}
+		return;
+	}
+
+	public function queryTable($table, $column_names = [])
+	{
+		$table_obj = $this->tables->$table;
+		$result = [];
+		$column_name_indexes = [];
+
+		foreach ($table_obj->column_data as $cd_key => $cd_value) {
+			foreach ($table_obj->columns as $column_key => $column_value) {
+				if (in_array($column_key, $column_names)) {
+					$result[$cd_key][$column_key] = $cd_value[$column_value - 1];
+				}
+			}
+		}
+
+
+		return $result;
+	}
+
+	public function printDb()
+	{
+		$row = 1;
+		if (($handle = fopen($this->file, "r")) !== FALSE) {
+			while (($data = fgetcsv($handle, 1000, $this->separator)) !== FALSE) {
+				$num = count($data);
+				echo "<p> $num fields in line $row: <br /></p>\n";
+				$row++;
+				for ($c=0; $c < $num; $c++) {
+					echo $data[$c] . "<br />\n";
+				}
+			}
+			fclose($handle);
+		}
+	}
+
+}
--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-# read-csv
-Read some CSV data and prepare it for database inserting
+# CSV Reader
 
-To-do: Make it a module.
+Read some CSV data and prepare it for database inserting. 
+
+Example data
+
+	Year,Make,Model,Description,Price
+	Title, "My database"
+	1997,Ford,E350,"ac, abs, moon",3000.00
+	1999,Chevy,"Venture ""Extended Edition""","",4900.00
+	1999,Chevy,"Venture ""Extended Edition, Very Large""",,5000.00
+	1996,Jeep,Grand Cherokee,"MUST SELL! air, moon roof, loaded",4799.00
+
+Usage example
+
+	$foo = new CSVReader('database.csv', ',');
+	$foo->setTable('details', 2)
+		->setTableColumns(['title' => 2])
+		->setTable('car_data', 3, $foo->theEnd())
+		->setTableColumns([
+			"Year" => 1, 
+			"Make" => 2, 
+			"Model" => 3, 
+			"Description" => 4, 
+			"Price"  => 5
+		]);
+
+	// Query the CSV file
+	echo '<pre>';
+	print_r($foo);
+	$bar = $foo->queryTable('details', ['title']);
+	$bar = $foo->queryTable('car_data', ['Year', 'Price']);
+
+### To-do
+
+Make it a module.
